43681405819dfa078e544a4a3107a0885c3cb424
sky1143 <skyup143@gmail.com>
2025-02-23 11:53:14 +0530

    updated: allowedHost to the webisite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", 
-    port: 5173, 
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["blogging-website-h0v6.onrender.com"], // Allow Render domain
   },
   build: {
-    outDir: "dist", 
+    outDir: "dist",
   },
 });
