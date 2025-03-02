2982844ffff49ef8aac47fef7f362f645009747f
sky1143 <skyup143@gmail.com>
2025-03-02 17:19:33 +0530

    update:change the api url for deployment

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -14,7 +14,7 @@ const Blog = () => {
     image: null, // Changed from imageUrl to image (file)
   });
 
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
 
   useEffect(() => {
