d06be0a9cf520ce85ef82c5324617f3e427605ac
sky1143 <skyup143@gmail.com>
2025-03-05 11:03:25 +0530

    updated: cors

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 
-
-app.use(cors({
-    origin: 'https://blogging-website-1-frontend.onrender.com/', 
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
-    allowedHeaders: ['Content-Type']
-  }));
+app.use(cors({ origin: '*' }));
 
 connectDB();
 
